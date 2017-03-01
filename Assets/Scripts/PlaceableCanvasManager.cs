9389687a9214abca99dcb93771cdbfa7a61963a6
calaleb <caleb@calebsEmail.com>
2017-03-01 09:53:35 +0200

    .

---
+++ Assets/Scripts/PlaceableCanvasManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceableCanvasManager : MonoBehaviour
+{
+
+    private Vector3 offsetVector;
+    private Quaternion defaultRotation;
+
+    public bool isPlaced;
+
+    void Start()
+    {
+        offsetVector = transform.position - Camera.main.transform.position;
+        defaultRotation = transform.rotation;
+    }
+
+    void FixedUpdate()
+    {
+        if (isPlaced)
+            return;
+        transform.position = Camera.main.transform.position + Camera.main.transform.TransformVector(offsetVector);
+        transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position) * defaultRotation;
+    }
+
+    public void PlaceCanvas()
+    {
+        isPlaced = true;
+        offsetVector = transform.position - Camera.main.transform.position;
+        defaultRotation = transform.rotation;
+    }
+
+    public void MoveCanvas()
+    {
+        isPlaced = false;
+    }
+}
