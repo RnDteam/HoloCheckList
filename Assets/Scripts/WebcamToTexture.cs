5fd2f21157dc515c9060b18ec687526fc8ebde5f
De-Panther <depanther@gmail.com>
2017-03-20 15:18:59 +0200

    Getting Webcam feed with Vuforia tests

---
+++ Assets/Scripts/WebcamToTexture.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class WebcamToTexture : MonoBehaviour, IVideoBackgroundEventHandler
+{
+	private Texture mVideoBackgroundTexture = null;
+	private bool mVideoBgConfigChanged = false;
+
+	// Use this for initialization
+	void Start () {
+		VuforiaARController.Instance.RegisterVideoBgEventHandler(this);
+	}
+
+	void OnDestroy()
+	{
+		// unregister for the OnVideoBackgroundConfigChanged event at the VuforiaBehaviour
+		VuforiaARController.Instance.UnregisterVideoBgEventHandler(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mVideoBgConfigChanged && VuforiaRenderer.Instance.IsVideoBackgroundInfoAvailable())
+		{
+			UpdateVideoTexture();
+			mVideoBgConfigChanged = false;
+		}
+	}
+
+	private void UpdateVideoTexture()
+	{
+		if (mVideoBackgroundTexture != VuforiaRenderer.Instance.VideoBackgroundTexture)
+		{
+			mVideoBackgroundTexture = VuforiaRenderer.Instance.VideoBackgroundTexture;
+			GetComponent<Renderer>().material.mainTexture = mVideoBackgroundTexture;
+		}
+	}
+
+	public void OnVideoBackgroundConfigChanged()
+	{
+		mVideoBgConfigChanged = true;
+	}
+}
---
+++ Assets/Scripts/WebcamToTexture2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamToTexture2 : MonoBehaviour {
+
+	void Start() {
+		WebCamTexture webcamTexture = new WebCamTexture();
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+}
