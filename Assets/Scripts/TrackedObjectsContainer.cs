f607656efba06b99056693af0734789d922b0df8
De-Panther <depanther@gmail.com>
2017-03-20 17:15:52 +0200

    Added TrackedObjectsContainer class and Dummy tracked objects on main scene for testing

---
+++ Assets/Scripts/TrackedObjectsContainer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackedObjectsContainer : MonoBehaviour {
+
+	public static TrackedObjectsContainer Instance;
+
+	private Dictionary<string, GameObject> trackedObjects = new Dictionary<string, GameObject>();
+
+	// Use this for initialization
+	void Awake () {
+		Instance = this;
+		foreach(Transform trackedObject in transform)
+		{
+			trackedObjects.Add(trackedObject.name, trackedObject.gameObject);
+		}
+		DisableAllTrackedObjects();
+	}
+
+	public void EnableTrackedObject(string objectName)
+	{
+		if (trackedObjects.ContainsKey(objectName))
+		{
+			trackedObjects[objectName].SetActive(true);
+		}
+	}
+
+	public void DisableTrackedObject(string objectName)
+	{
+		if (trackedObjects.ContainsKey(objectName))
+		{
+			trackedObjects[objectName].SetActive(false);
+		}
+	}
+
+	public void DisableAllTrackedObjects()
+	{
+		foreach (var trackedObject in trackedObjects)
+		{
+			trackedObject.Value.SetActive(false);
+		}
+	}
+	
+}
