e3dfea7ace150beb284ea9446ebee3e4cda0b156
De-Panther <depanther@gmail.com>
2017-03-23 19:05:20 +0200

    Added new OneTaskPanel enter/exit animation

--- Assets/Scripts/OneTaskController.cs
+++ Assets/Scripts/OneTaskController.cs
@@ -6,9 +6,20 @@ using UnityEngine.UI;
 public class OneTaskController : MonoBehaviour {
 
 	public GameObject TaskParent;
-	public TaskStrip TaskStrip;
-	public ContentSizeFitter sizeFitter;
+	public TaskStrip[] TaskStrips;
+	private RectTransform[] TaskStripsRectTransform = new RectTransform[2];
 	private bool updateSize = false;
+
+	public Vector3 inStartPos;
+	public Vector3 inEndPos;
+	public Vector3 outStartPos;
+	public Vector3 outEndPos;
+
+	public float enterSpeed = 1f;
+
+	private bool animate=false;
+	private float index=0;
+	private float rate;
 
 	void OnEnable()
 	{
@@ -26,8 +37,8 @@ public class OneTaskController : MonoBehaviour {
 
 	void StartTasks()
 	{
-		ShowTask();
 		ChangeTask();
+		ShowTask();
 	}
 
 	void EndTasks()
@@ -49,21 +60,58 @@ public class OneTaskController : MonoBehaviour {
 
 	void SetTask()
 	{
+		if (!TaskParent.activeSelf)
+		{
+			TaskStrips[0].gameObject.SetActive(true);
+			TaskStrips[1].gameObject.SetActive(false);
+		}
+		else
+		{
+			TaskStrip temp = TaskStrips[0];
+			TaskStrips[0] = TaskStrips[1];
+			TaskStrips[1] = temp;
+			temp = null;
+		}
+		TaskStripsRectTransform[0] = TaskStrips[0].GetComponent<RectTransform>();
+		TaskStripsRectTransform[1] = TaskStrips[1].GetComponent<RectTransform>();
+
 		Task task = TaskManager.CurrentTask;
-		TaskStrip.SetTaskText(task.instruction);
-		TaskStrip.ShowInfoIcon(!string.IsNullOrEmpty(task.hasExtraInfo));
-		TaskStrip.ShowValidationIcon(task.signedTask);
-		TaskStrip.SetValidated(true);
+		TaskStrips[0].SetTaskText(task.instruction);
+		TaskStrips[0].ShowInfoIcon(!string.IsNullOrEmpty(task.hasExtraInfo));
+		TaskStrips[0].ShowValidationIcon(task.signedTask);
+		TaskStrips[0].SetValidated(true);
+
 		updateSize = true;
+
+		animate = true;
+		index = 0;
+		rate = 1.0f/enterSpeed;
 	}
 
 	void Update()
 	{
-		if (updateSize)
+		if (animate)
 		{
-			updateSize = false;
-			sizeFitter.enabled = false;
-			sizeFitter.enabled = true;
+			if(index<1.0f)
+			{
+				if(index == 0)
+				{
+					TaskStrips[0].gameObject.SetActive(false);//SetTaskText(TaskStrips[0].TaskText.text);
+				}
+				else if (updateSize)
+				{
+					updateSize = false;
+					TaskStrips[0].gameObject.SetActive(true);
+				}
+				TaskStripsRectTransform[0].anchoredPosition3D = Vector3.Lerp(inStartPos, inEndPos, index);
+				TaskStripsRectTransform[1].anchoredPosition3D = Vector3.Lerp(outStartPos, outEndPos, index);
+				index+=rate*Time.deltaTime;
+			}else{
+				animate=false;
+				TaskStripsRectTransform[0].anchoredPosition3D = inEndPos;
+				TaskStripsRectTransform[1].anchoredPosition3D = outEndPos;
+				index=1.0f;
+			}
 		}
 	}
 
