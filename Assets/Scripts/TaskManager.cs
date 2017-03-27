0cc26df58f9201c43eb14ed3f491106bcf0de6e6
yarivg <yarivgavriel2@gmail.com>
2017-03-27 10:33:33 +0300

    Add stats to end scene

--- Assets/Scripts/TaskManager.cs
+++ Assets/Scripts/TaskManager.cs
@@ -12,43 +12,56 @@ public class TaskManager : MonoBehaviour {
     public static event TaskChange OnCardChanged;
 
     private static Card[] cards;
-    public static int nCardsNumber;
-    public static int nCardIndex;
-    public static int nTaskIndex;
     public static Task PreviousTask;
+
+    public static int CardsNumber { get; private set; }
+    public static int CardIndex { get; private set; }
+    public static int TaskIndex { get; private set; }
+    public static int TasksToSign { get; private set; }
 
     public static Task CurrentTask
     {
         get {
-			return (nCardIndex < cards.Length && nTaskIndex < CurrentCard.tasks.Length) ? cards[nCardIndex].tasks[nTaskIndex] : null;
+			return (CardIndex < cards.Length && TaskIndex < CurrentCard.tasks.Length) ? cards[CardIndex].tasks[TaskIndex] : null;
 		}
     }
 
     public static Card CurrentCard
     {
-        get { return nCardIndex < cards.Length ? cards[nCardIndex] : null; }
+        get { return CardIndex < cards.Length ? cards[CardIndex] : null; }
     }
 
     void Awake () {
+        InitTaskManager();
+    }
+
+    private static void InitTaskManager()
+    {
         cards = TextsBridge.GetCards();
-        nCardsNumber = cards.Length;
-        PreviousTask = CurrentTask;
-        nCardIndex = 0;
-        nTaskIndex = 0;
+
+        if (cards != null)
+        {
+            CalcNumberOfTaskToSign();
+            CardsNumber = cards.Length;
+            PreviousTask = CurrentTask;
+            CardIndex = 0;
+            TaskIndex = 0;
+        }
     }
 
     private void Start()
     {
-        if(OnStartTasks != null) 
+        if(PreviousTask == null) PreviousTask = CurrentTask;
+        if (OnStartTasks != null) 
             OnStartTasks();
     }
 
     public static bool nextTask()
     {
         PreviousTask = CurrentTask;
-        nTaskIndex++;
+        TaskIndex++;
 		bool isSameCard = true;
-		if (nTaskIndex >= CurrentCard.tasks.Length)
+		if (TaskIndex >= CurrentCard.tasks.Length)
 		{
 			nextCard();
 			isSameCard = false;
@@ -65,8 +78,8 @@ public class TaskManager : MonoBehaviour {
 
     public static void nextCard()
     {
-        nCardIndex++;
-        nTaskIndex = 0;
+        CardIndex++;
+        TaskIndex = 0;
 
         if (OnCardChanged != null)
             OnCardChanged();
@@ -78,6 +91,28 @@ public class TaskManager : MonoBehaviour {
 
     public static bool isFinished()
     {
-        return nCardIndex >= cards.Length;
+        return CardIndex >= cards.Length;
+    }
+
+    private static void CalcNumberOfTaskToSign()
+    {
+        TasksToSign = 0;
+
+        foreach (Card c in cards)
+            foreach (Task t in c.tasks)
+                TasksToSign += t.signedTask ? 1 : 0;
+    }
+
+    public static int GetNumberOfSignedTasks()
+    {
+        int signedTasks = 0;
+
+        if (cards == null) InitTaskManager();
+
+        foreach (Card c in cards)
+            foreach (Task t in c.tasks)
+                signedTasks += t.isAlreadySigned ? 1 : 0;
+
+        return signedTasks;
     }
 }
